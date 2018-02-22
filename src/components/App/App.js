77b3705515622c7fcc0cef6ef7fabfd6960e3fde
Jordan Quinn <jpquinn605@gmail.com>
2018-02-22 09:39:40 -0700

    wolf loading gif" "

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes, { shape, func, string } from 'prop-types';
 import logo from './logo.svg';
+import wolf from './wolf.gif';
 import './App.css';
 import { fetchApi } from '../../Utilities/api-helper'
 import { connect } from 'react-redux';
 import { storeHouses } from '../../actions';
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loaded: false
+    }
+
+  }
 
   async componentDidMount() {
     const houseData = await fetchApi('http://localhost:3001/api/v1/houses')
     this.props.storeHouses(houseData)
+    setTimeout( () => {
+      this.setState({
+      loaded: true
+    })
+    }, 2000)
   }
 
   render() {
@@ -19,6 +33,9 @@ class App extends Component {
           <img src={logo} className='App-logo' alt='logo' />
           <h2>Welcome to Westeros</h2>
         </div>
+        { !this.state.loaded && 
+          <img src={wolf} id='wolf' alt='loading-gif' />
+        }
         <div className='Display-info'>
         </div>
       </div>
